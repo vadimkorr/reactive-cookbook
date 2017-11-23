289a7b9d4c1de9b6760c835deb555991e6acef2f
Vadim <korobeinikovv@mail.ru>
2017-11-23 09:48:14 +0300

    Added prop types for dropdown

--- src/components/array-dropdown/array-dropdown.js
+++ src/components/array-dropdown/array-dropdown.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './array-dropdown.css';
 
 import FormControl  from 'react-bootstrap/lib/FormControl';
 
 class ArrayDropdown extends Component {
+    propTypes = {
+        arr: PropTypes.array.isRequired
+    };
+
     constructor(props) {
         super(props);
         console.log(props);
